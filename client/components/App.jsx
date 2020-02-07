09c42297f7780726797d8a558584e3a636e7cf13
JillGlass <glass.nz@gmail.com>
2020-02-07 15:21:06 +1300

    add in example styled components to Home - need to update these

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,28 +1,10 @@
 import React from "react";
-import Theme from "./Theme";
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: 100%;
-  border: ${props => `1px solid ${props.theme.colors.onyx}`};
-  background-color: ${props => props.theme.colors.lightBlue};
-  font-family: ${props => props.theme.fonts[0]};
-`;
-
-const Heading = styled.h1`
-  font-size: ${({ isHeading, theme: { fontSizes } }) =>
-    isHeading ? fontSizes.large : fontSizes.small};
-  color: ${({ theme: { colors } }) => colors.persianGreen};
-`;
+import Home from './Home'
 
 const App = () => {
   return (
-    <Theme>
-      <Container>
-        <Heading isHeading={true}>Hello World</Heading>
-        <h2>By the power of styled-components!</h2>
-      </Container>
-    </Theme>
+
+    <Home />
   );
 };
 export default App;
