eb66edf16329040c78210eeda9c36f1995ffa82b
JillGlass <glass.nz@gmail.com>
2020-04-02 15:02:46 +1300

    install upload icon, need to style upload button

--- client/components/UploadPhoto.jsx
+++ client/components/UploadPhoto.jsx
@@ -1,20 +1,28 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
- 
-export default function UploadPhoto() {
-  const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
- 
+
+import React from 'react';
+import {useDropzone} from 'react-dropzone';
+import PublishIcon from '@material-ui/icons/Publish';
+
+export default function UploadPhoto(props) {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
+    <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <PublishIcon className="icon-upload"/>
+        <p>Click to select a stall photo to upload</p>
+      </div>
+      <aside>
+        
+        {/* <ul>{files}</ul> */}
+      </aside>
+    </section>
+  );
 }
